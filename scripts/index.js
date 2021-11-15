10371653e7a38b7cd5c49ba375c97a4e328152f7
Artem Schirov <artemschirov@gmail.com>
2021-11-16 01:57:05 +0200

    feat: popup open with click and close

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,17 @@
 console.log('loaded');
-
 const editBtn = document.querySelector('.profile__edit-btn');
-console.log(editBtn);
-
 const popup = document.querySelector('.popup');
-console.log(popup);
-
 const popupCloseBtn = popup.querySelector('.popup__close');
-console.log(popupCloseBtn);
-
+const popupOverlay = popup.querySelector('.popup__overlay');
 
 function open() {
-    popup.classList.add('.popup_opened');
+    popup.classList.add('popup_opened');
 }
 
 const close = () => {
-    popup.classList.remove('.popup_opened');
+    popup.classList.remove('popup_opened');
 }
 
 editBtn.addEventListener('click', open);
 popupCloseBtn.addEventListener('click', close);
+popupOverlay.addEventListener('click', close);
