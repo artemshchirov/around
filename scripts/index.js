c0ba80c3fff2e6b7e214fabab3610d0e99021b9f
Artem Schirov <artemschirov@gmail.com>
2021-11-17 17:09:47 +0200

    fix: delete ovelay from script.js

--- scripts/index.js
+++ scripts/index.js
@@ -2,25 +2,23 @@
 const editBtn = document.querySelector('.profile__edit-btn');
 const popupElement = document.querySelector('.popup');
 const popupCloseBtn = popupElement.querySelector('.popup__close');
-const popupOverlay = popupElement.querySelector('.popup__overlay');
+// const popupOverlay = popupElement.querySelector('.popup__overlay');
 
 function open() {
     popupElement.classList.add('popup_opened');
 }
-
 const close = () => {
     popupElement.classList.remove('popup_opened');
 }
 
 editBtn.addEventListener('click', open);
 popupCloseBtn.addEventListener('click', close);
-popupOverlay.addEventListener('click', close);
+// popupOverlay.addEventListener('click', close);
 
 // save inputs and close popup
 const profile = document.querySelector('.profile');
 let username = profile.querySelector('.profile__name');
 let about = profile.querySelector('.profile__about');
-
 let formElement = popupElement.querySelector('.form');
 let usernameInput = formElement.querySelectorAll('.form__input')[0];
 let aboutInput = formElement.querySelectorAll('.form__input')[1];
