b8f06109a57aa92d599957ff87c60da7996075e5
Artem Schirov <artemschirov@gmail.com>
2021-11-18 23:51:57 +0200

    fix: fix review issues

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,28 @@
-// open, close popup
 const editBtn = document.querySelector('.button_profile_edit');
 const popupElement = document.querySelector('.popup');
 const popupCloseBtn = popupElement.querySelector('.popup__close');
+const profile = document.querySelector('.profile');
+const username = profile.querySelector('.profile__name');
+const about = profile.querySelector('.profile__about');
+const formElement = popupElement.querySelector('.form');
+const usernameInput = document.getElementById('name-edit_input_id');
+const aboutInput = document.getElementById('about-edit_input_id');
 
-function open() {
+function openEditProfilePopup() {
+    usernameInput.value = username.textContent.trim();
+    aboutInput.value = about.textContent.trim();
     popupElement.classList.add('popup_opened');
 }
-const close = () => {
+const closeEditProfilePopup = () => {
     popupElement.classList.remove('popup_opened');
 }
-
-editBtn.addEventListener('click', open);
-popupCloseBtn.addEventListener('click', close);
-
-// save inputs and close popup
-const profile = document.querySelector('.profile');
-let username = profile.querySelector('.profile__name');
-let about = profile.querySelector('.profile__about');
-let formElement = popupElement.querySelector('.form');
-let usernameInput = formElement.querySelectorAll('.form__input')[0];
-let aboutInput = formElement.querySelectorAll('.form__input')[1];
-
-function save(evt) {
+function formSubmitHandler(evt) {
     evt.preventDefault();
-    console.log('save');
     username.textContent = usernameInput.value; 
     about.textContent = aboutInput.value;
-    close();
+    closeEditProfilePopup();
 }
 
-formElement.addEventListener('submit', save); 
+editBtn.addEventListener('click', openEditProfilePopup);
+popupCloseBtn.addEventListener('click', closeEditProfilePopup);
+formElement.addEventListener('submit', formSubmitHandler); 
