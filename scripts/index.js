c32796791e1879d2f3a281838de083b2ac2e4d43
Artem Schirov <artemschirov@gmail.com>
2021-11-23 00:27:25 +0200

    feat: remove popup animation

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,30 @@
-const editBtn = document.querySelector('.button_profile_edit');
-const popupElement = document.querySelector('.popup');
-const popupCloseBtn = popupElement.querySelector('.button_form_close');
-const profile = document.querySelector('.profile');
-const username = profile.querySelector('.profile__name');
-const about = profile.querySelector('.profile__about');
-const formElement = popupElement.querySelector('.form');
-const usernameInput = document.getElementById('name-edit_input_id');
-const aboutInput = document.getElementById('about-edit_input_id');
+const editBtn = document.querySelector(".button_profile_edit");
+const popupElement = document.querySelector(".popup");
+const popupCloseBtn = popupElement.querySelector(".button_form_close");
+const profile = document.querySelector(".profile");
+const username = profile.querySelector(".profile__name");
+const about = profile.querySelector(".profile__about");
+const formElement = popupElement.querySelector(".form");
+const usernameInput = document.getElementById("name-edit_input_id");
+const aboutInput = document.getElementById("about-edit_input_id");
 
 function openEditProfilePopup() {
     usernameInput.value = username.textContent.trim();
     aboutInput.value = about.textContent.trim();
-    popupElement.classList.add('popup_opened');
+    popupElement.classList.add("popup_opened");
 }
+
 const closeEditProfilePopup = () => {
-    popupElement.classList.remove('popup_opened');
-}
+    popupElement.classList.remove("popup_opened");
+};
+
 function formSubmitHandler(evt) {
     evt.preventDefault();
-    username.textContent = usernameInput.value; 
+    username.textContent = usernameInput.value;
     about.textContent = aboutInput.value;
     closeEditProfilePopup();
 }
 
-editBtn.addEventListener('click', openEditProfilePopup);
-popupCloseBtn.addEventListener('click', closeEditProfilePopup);
-formElement.addEventListener('submit', formSubmitHandler); 
+editBtn.addEventListener("click", openEditProfilePopup);
+popupCloseBtn.addEventListener("click", closeEditProfilePopup);
+formElement.addEventListener("submit", formSubmitHandler);
