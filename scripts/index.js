e2cba78360c139c98ca34976c58d3bde4a7cf345
Artem Schirov <artemschirov@gmail.com>
2021-12-16 01:27:16 +0200

    fix: review errors

--- scripts/index.js
+++ scripts/index.js
@@ -8,126 +8,125 @@ const usernameInput = document.getElementById('name-edit');
 const aboutInput = document.getElementById('about-edit');
 const popupAddCard = document.querySelector('.popup_card-add');
 const formAddCard = popupAddCard.querySelector('.form')
+const createCardButton = formAddCard.querySelector('.button_form_submit');
 const addCardBtn = document.querySelector('.button_profile_add');
 const newCardNameInput = document.getElementById('name-card');
 const newCardLinkInput = document.getElementById('link-card');
 const cards = document.querySelector('.cards');
 const cardTemplate = document.querySelector('#card').content;
 const popupCardFullscreen = document.querySelector('.popup_card-fullscreen');
+const imageCardFullscreen = popupCardFullscreen.querySelector('.popup__image');
+const titleCardFullscreen = popupCardFullscreen.querySelector('.popup__title');
 const popupCloseBtns = Array.from(document.querySelectorAll('.button_popup_close'));
 const popupOverlays = Array.from(document.querySelectorAll('.popup__overlay'));
 const popups = Array.from(document.querySelectorAll('.popup'));
 const initialCards = [
-    {
-        name: 'Мордор',
-        link: './images/mordor.jpg',
-        alt: 'Мордор — область на юго-востоке Средиземья к востоку от Андуина, владения Саурона',
-    },
-    {
-        name: 'Лотлориэн',
-        link: './images/lothlorien.jpg',
-        alt: 'Обширный разноцветный лес. Среди деревьев виднеется эльфийский город',
-    },
-    {
-        name: 'Столпы Аргоната',
-        link: './images/lotr.jpg',
-        alt: 'Столпы Аргоната - — монумент, состоящий из двух огромной величины статуй, высеченных в скалах и изображающих Исильдура и Анариона, стоящих по обе стороны реки Андуин на северных подходах к Нен Хитоэль.',
-    },
-    {
-        name: 'г. Мория',
-        link: './images/moria.jpeg',
-        alt: 'гора Мория - большая пещера с каменным мостом над бездной',
-    },
-    {
-        name: 'Ривенделл',
-        link: './images/rivendell.jpg',
-        alt: 'Ривенделл - эльфийский город с высокими белыми башнями в горах, покрытых лесом',
-    },
-    {
-        name: 'Шир',
-        link: './images/shire.jpg',
-        alt: 'Шир - деревня хоббитов с об   устроенные домами-норами внутри холмов',
-    },
+  {
+    name: 'Мордор',
+    link: './images/mordor.jpg',
+    alt: 'Мордор — область на юго-востоке Средиземья к востоку от Андуина, владения Саурона',
+  },
+  {
+    name: 'Лотлориэн',
+    link: './images/lothlorien.jpg',
+    alt: 'Обширный разноцветный лес. Среди деревьев виднеется эльфийский город',
+  },
+  {
+    name: 'Столпы Аргоната',
+    link: './images/lotr.jpg',
+    alt: 'Столпы Аргоната - — монумент, состоящий из двух огромной величины статуй, высеченных в скалах и изображающих Исильдура и Анариона, стоящих по обе стороны реки Андуин на северных подходах к Нен Хитоэль.',
+  },
+  {
+    name: 'г. Мория',
+    link: './images/moria.jpeg',
+    alt: 'гора Мория - большая пещера с каменным мостом над бездной',
+  },
+  {
+    name: 'Ривенделл',
+    link: './images/rivendell.jpg',
+    alt: 'Ривенделл - эльфийский город с высокими белыми башнями в горах, покрытых лесом',
+  },
+  {
+    name: 'Шир',
+    link: './images/shire.jpg',
+    alt: 'Шир - деревня хоббитов с обустроенными домами-норами внутри холмов',
+  },
 ];
 
 const handleKey = evt => {
-    const popupOpened = document.querySelector('.popup_opened');
-    if (popupOpened && evt.key === "Escape") closePopup(popupOpened);
+  const popupOpened = document.querySelector('.popup_opened');
+  if (popupOpened && evt.key === "Escape") closePopup(popupOpened);
 }
 const openPopup = popup => {
-    document.addEventListener('keydown', handleKey);
-    popup.classList.add('popup_opened');
+  document.addEventListener('keydown', handleKey);
+  popup.classList.add('popup_opened');
 }
 const closePopup = popup => {
-    document.removeEventListener('keydown', handleKey);
-    popup.classList.remove('popup_opened');
+  document.removeEventListener('keydown', handleKey);
+  popup.classList.remove('popup_opened');
 }
 const fillInputsUserData = () => {
-    usernameInput.value = username.textContent.trim();
-    aboutInput.value = about.textContent.trim();
+  usernameInput.value = username.textContent.trim();
+  aboutInput.value = about.textContent.trim();
 }
 const openProfileEditPopup = () => {
-    openPopup(popupProfileEdit);
-    fillInputsUserData();
+  fillInputsUserData();
+  openPopup(popupProfileEdit);
 }
-const refreshProfile = () => {
-    username.textContent = usernameInput.value;
-    about.textContent = aboutInput.value;
+const updateProfile = () => {
+  username.textContent = usernameInput.value;
+  about.textContent = aboutInput.value;
 }
-const formProfileEditHandler = evt => {
-    evt.preventDefault();
-    refreshProfile();
-    closePopup(evt.target.closest('.popup_opened'));
+const formProfileEditHandler = () => {
+  updateProfile();
+  closePopup(popupProfileEdit);
 }
-const formAddCardHandler = evt => {
-    evt.preventDefault();
-    addCard(newCardNameInput.value, newCardLinkInput.value);
-    closePopup(evt.target.closest('.popup_opened'));
-    newCardNameInput.value = "";
-    newCardLinkInput.value = "";
+const formAddCardHandler = () => {
+  const userCard = {
+    name: newCardNameInput.value,
+    link: newCardLinkInput.value,
+    alt: `Картинка пользователя: "${newCardNameInput.value}"`,
+  };
+  addCard(userCard);
+  closePopup(popupAddCard);
+  formAddCard.reset();
+  disableButton(createCardButton, validationObj.inactiveButtonClass);
 }
-const addCard = (title, src) => {
-    const newCard = createCard({ name: title, link: src });
-    cards.prepend(newCard);
+const addCard = card => {
+  const newCard = createCard(card);
+  cards.prepend(newCard);
 }
 const changeLikeStatus = item => {
-    if (item.style.backgroundImage.includes('like_active.svg')) {
-        item.style.backgroundImage = 'url("./images/like_disabled.svg")';
-    } else {
-        item.style.backgroundImage = 'url("./images/like_active.svg")';
-    };
+  item.classList.toggle('button_like_isLiked');
 }
 const deleteCard = evt => {
-    evt.target.closest('.card').remove();
+  evt.target.closest('.card').remove();
 }
-const openCardFullscreen = evt => {
-    eventCard = evt.target.closest('.card');
-    popupCardFullscreen.querySelector('.popup__image').src = eventCard.querySelector('.card__image').src;
-    popupCardFullscreen.querySelector('.popup__image').alt = eventCard.querySelector('.card__image').alt;
-    popupCardFullscreen.querySelector('.popup__title').textContent = evt.textContent;
-    openPopup(popupCardFullscreen);
+const openCardFullscreen = card => {
+  imageCardFullscreen.src = card.link;
+  imageCardFullscreen.alt = card.alt;
+  titleCardFullscreen.textContent = card.name;
+  openPopup(popupCardFullscreen);
 }
 const createCard = card => {
-    const cardElem = cardTemplate.querySelector('.card').cloneNode(true);
-    const cardTitle = cardElem.querySelector('.card__title');
-    const cardImage = cardElem.querySelector('.card__image');
-    const deleteBtn = cardElem.querySelector('.button_card_delete');
-    const likeBtn = cardElem.querySelector('.button_like');
-    cardTitle.textContent = card.name;
-    cardImage.src = card.link;
-    cardImage.alt = `Картинка пользователя: ${cardElem.textContent.trim()}`;
-    cardImage.addEventListener('click', openCardFullscreen);
-    deleteBtn.addEventListener('click', deleteCard);
-    likeBtn.addEventListener('click', () => changeLikeStatus(likeBtn));
-    return cardElem;
+  const cardElem = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardTitle = cardElem.querySelector('.card__title');
+  const cardImage = cardElem.querySelector('.card__image');
+  const deleteBtn = cardElem.querySelector('.button_card_delete');
+  const likeBtn = cardElem.querySelector('.button_like');
+  cardTitle.textContent = card.name;
+  cardImage.src = card.link;
+  cardImage.alt = card.alt;
+  cardImage.addEventListener('click', () => openCardFullscreen(card));
+  likeBtn.addEventListener('click', () => changeLikeStatus(likeBtn));
+  deleteBtn.addEventListener('click', deleteCard);
+  return cardElem;
 }
-
-fillInputsUserData();
 
 editProfileBtn.addEventListener('click', openProfileEditPopup);
 addCardBtn.addEventListener('click', () => openPopup(popupAddCard));
 formProfileEdit.addEventListener('submit', formProfileEditHandler);
 formAddCard.addEventListener('submit', formAddCardHandler);
-initialCards.forEach(card => cards.prepend(createCard(card)));
+initialCards.forEach(card => addCard(card));
 popupCloseBtns.forEach(btn => btn.addEventListener('click', evt => closePopup(evt.target.closest('.popup_opened'))));
 popupOverlays.forEach(overlay => overlay.addEventListener('click', evt => closePopup(evt.target.closest('.popup_opened'))));
--- scripts/validation.js
+++ scripts/validation.js
@@ -1,60 +1,66 @@
 const validationObj = {
-    formSelector: '.form',
-    inputSelector: '.form__input',
-    submitButtonSelector: '.button_form_submit',
-    inactiveButtonClass: 'button_disabled',
-    inputErrorClass: 'form__input_type_error',
-    errorClass: 'form__input-error_visible'
+  formSelector: '.form',
+  inputSelector: '.form__input',
+  submitButtonSelector: '.button_form_submit',
+  inactiveButtonClass: 'button_disabled',
+  inputErrorClass: 'form__input_type_error',
+  errorClass: 'form__input-error_visible'
 }
 
 const showInputError = (formElem, inputElem, errorMessageText, errorMessageClass, inputErrorClass) => {
-    const errorElem = formElem.querySelector(`#${inputElem.id}-error`);
-    errorElem.textContent = errorMessageText;
-    errorElem.classList.add(errorMessageClass);
-    inputElem.classList.add(inputErrorClass);
+  const errorElem = formElem.querySelector(`#${inputElem.id}-error`);
+  errorElem.textContent = errorMessageText;
+  errorElem.classList.add(errorMessageClass);
+  inputElem.classList.add(inputErrorClass);
 }
 const hideInputError = (formElem, inputElem, errorMessageClass, inputErrorClass) => {
-    const errorElem = formElem.querySelector(`#${inputElem.id}-error`);
-    errorElem.textContent = '';
-    errorElem.classList.remove(errorMessageClass);
-    inputElem.classList.remove(inputErrorClass);
+  const errorElem = formElem.querySelector(`#${inputElem.id}-error`);
+  errorElem.textContent = '';
+  errorElem.classList.remove(errorMessageClass);
+  inputElem.classList.remove(inputErrorClass);
 }
 const checkInputValidity = (formElem, inputElem, { inputErrorClass, errorClass }) => {
-    if (!inputElem.validity.valid) {
-        showInputError(formElem, inputElem, inputElem.validationMessage, errorClass, inputErrorClass);
-    } else {
-        hideInputError(formElem, inputElem, errorClass, inputErrorClass);
-    };
+  if (!inputElem.validity.valid) {
+    showInputError(formElem, inputElem, inputElem.validationMessage, errorClass, inputErrorClass);
+  } else {
+    hideInputError(formElem, inputElem, errorClass, inputErrorClass);
+  };
 }
 const hasInvalidInput = inputList => {
-    return inputList.some(inputElem => !inputElem.validity.valid);
+  return inputList.some(inputElem => !inputElem.validity.valid);
+}
+const disableButton = (button, inactiveButtonClass) => {
+  button.disabled = true;
+  button.classList.add(inactiveButtonClass);
+}
+const enableButton = (button, inactiveButtonClass) => {
+  button.disabled = false;
+  button.classList.remove(inactiveButtonClass);
 }
 const toggleButtonState = (inputList, buttonElement, inactiveButtonClass) => {
-    if (hasInvalidInput(inputList)) {
-        buttonElement.disabled = true;
-        buttonElement.classList.add(inactiveButtonClass);
-    } else {
-        buttonElement.disabled = false;
-        buttonElement.classList.remove(inactiveButtonClass);
-    };
+  if (hasInvalidInput(inputList)) {
+    disableButton(buttonElement, inactiveButtonClass);
+  } else {
+    enableButton(buttonElement, inactiveButtonClass);
+  };
 }
 const setEventListeners = (formElem, { inputSelector, submitButtonSelector, inactiveButtonClass, ...rest }) => {
-    const inputList = Array.from(formElem.querySelectorAll(inputSelector));
-    const buttonElem = formElem.querySelector(submitButtonSelector);
-    toggleButtonState(inputList, buttonElem, inactiveButtonClass);
-    inputList.forEach(inputElem => {
-        inputElem.addEventListener('input', () => {
-            checkInputValidity(formElem, inputElem, rest);
-            toggleButtonState(inputList, buttonElem, inactiveButtonClass);
-        });
+  const inputList = Array.from(formElem.querySelectorAll(inputSelector));
+  const buttonElem = formElem.querySelector(submitButtonSelector);
+  toggleButtonState(inputList, buttonElem, inactiveButtonClass);
+  inputList.forEach(inputElem => {
+    inputElem.addEventListener('input', () => {
+      checkInputValidity(formElem, inputElem, rest);
+      toggleButtonState(inputList, buttonElem, inactiveButtonClass);
     });
+  });
 }
 const enableValidation = ({ formSelector, ...rest }) => {
-    const formList = Array.from(document.querySelectorAll(formSelector));
-    formList.forEach(formElem => {
-        formElem.addEventListener('submit', evt => evt.preventDefault());
-        setEventListeners(formElem, rest);
-    });
+  const formList = Array.from(document.querySelectorAll(formSelector));
+  formList.forEach(formElem => {
+    formElem.addEventListener('submit', evt => evt.preventDefault());
+    setEventListeners(formElem, rest);
+  });
 }
 
 enableValidation(validationObj);
